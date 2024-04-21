a65430705ba5690576f3ae24183d532ef3b9200c
Felix <felixdressler@freenet.de>
2024-04-21 19:58:48 +0200

    intial folder setup

---
+++ lib/app/fixedfundsflowapp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FixedFundsFlowApp extends StatelessWidget {
+  const FixedFundsFlowApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello Pogo'),
+        ),
+      ),
+    );
+  }
+}
