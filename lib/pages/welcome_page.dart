f7d45623dfedb546a41510cee727702f75971043
Felix <felixdressler@freenet.de>
2024-04-22 19:21:09 +0200

    Fixup Themes

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -5,8 +5,9 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: const Center(
         child: Text('Hello Pogo Intro'),
       ),
     );
