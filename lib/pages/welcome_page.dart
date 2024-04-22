6fc8159ed0c15651a883452d65a025cc81f5729d
Felix <felixdressler@freenet.de>
2024-04-22 22:43:50 +0200

    Appbar, Floating Icon, without methods

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -6,9 +6,34 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //acting like im pressed
+            },
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: const Center(
         child: Text('Hello Pogo Intro'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //acting like im pressed
+        },
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
       ),
     );
   }
