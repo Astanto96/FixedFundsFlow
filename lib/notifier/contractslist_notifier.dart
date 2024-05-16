a375f6f17d8a46f89bd5618a0d1ea8afce651226
Felix <felixdressler@freenet.de>
2024-05-16 07:18:19 +0200

    updated Contract

--- lib/notifier/contractslist_notifier.dart
+++ lib/notifier/contractslist_notifier.dart
@@ -1,11 +1,30 @@
+import 'package:fixedfundsflow/model/billing_period.dart';
+import 'package:fixedfundsflow/model/category.dart';
 import 'package:fixedfundsflow/model/contract.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ContractlistNotifier extends StateNotifier<List<Contract>> {
   ContractlistNotifier() : super([]);
 
-  void addVertrag(Contract vertrag) {
-    state = [...state, vertrag];
+  void addContract(
+      String description,
+      BillingPeriod billingPeriod,
+      DateTime? contractStart,
+      DateTime? contractEnd,
+      bool openEnd,
+      Category category,
+      bool income) {
+    final newContract = Contract(
+      id: DateTime.now().millisecondsSinceEpoch,
+      description: description,
+      billingPeriod: billingPeriod,
+      contractStart: contractStart,
+      contractEnd: contractEnd,
+      openEnd: openEnd,
+      category: category,
+      income: income,
+    );
+    state = [...state, newContract];
   }
 
   void updateVertrag(int id, Contract updatedVertrag) {
