eb11d516d26fdb0dff997cd0ba80d16e52a10ed4
Felix <felixdressler@freenet.de>
2024-05-16 07:59:00 +0200

    dummycategorys

--- lib/notifier/categorys_notifier.dart
+++ lib/notifier/categorys_notifier.dart
@@ -2,7 +2,15 @@ import 'package:fixedfundsflow/model/category.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CategoryslistNotifier extends StateNotifier<List<Category>> {
-  CategoryslistNotifier() : super([]);
+  CategoryslistNotifier()
+      : super([
+          Category(id: 1, description: 'Category 1'),
+          Category(id: 2, description: 'Category 2'),
+          Category(id: 3, description: 'Category 3'),
+          Category(id: 4, description: 'Category 4'),
+          Category(id: 5, description: 'Category 5'),
+          Category(id: 6, description: 'Category 6')
+        ]);
 
   void addCategory(Category category) {
     state = [...state, category];
