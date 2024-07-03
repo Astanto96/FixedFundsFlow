61d240a9a2f644c100ba745e313abb49d94faab1
Felix <felixdressler@freenet.de>
2024-07-03 17:50:15 +0200

    CategoryDialog + Notes

--- lib/widgets/category_form_dialog.dart
+++ lib/widgets/category_form_dialog.dart
@@ -1,3 +1,5 @@
+import 'package:fixedfundsflow/provider/categoryslist_provider.dart';
+import 'package:fixedfundsflow/widgets/custom_global_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +16,102 @@ class CategoryFormDialogState extends ConsumerState<CategoryFormDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 260),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Text(
+                "New Category",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter a Description",
+                      ),
+                      onSaved: (newValue) {
+                        _description = newValue ?? "";
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter a description";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 38, vertical: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                  Theme.of(context).colorScheme.primary),
+                              foregroundColor: MaterialStatePropertyAll(
+                                  Theme.of(context).colorScheme.inversePrimary),
+                              padding: const MaterialStatePropertyAll(
+                                EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 30),
+                              ),
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+
+                                ref
+                                    .read(categoryslistProvider.notifier)
+                                    .addCategory(_description);
+                                CustomGlobalSnackBar.show(context,
+                                    "Category successfuly created!", null);
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            child: const Icon(Icons.add_task_rounded),
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                  Theme.of(context).colorScheme.primary),
+                              foregroundColor: MaterialStatePropertyAll(
+                                  Theme.of(context).colorScheme.inversePrimary),
+                              padding: const MaterialStatePropertyAll(
+                                EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 30),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Icon(Icons.close_rounded),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
