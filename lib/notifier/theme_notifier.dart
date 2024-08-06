0925c008e90c3773e0fea5eaa32f1472b6599760
Felix <felixdressler@freenet.de>
2024-08-06 02:30:19 +0200

    Theme gets initially decided by the platformbrightness

--- lib/notifier/theme_notifier.dart
+++ lib/notifier/theme_notifier.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
 
 class ThemeNotifier extends StateNotifier<ThemeData> {
-  ThemeNotifier() : super(lightMode);
+  ThemeNotifier() : super(_getInitialTheme());
+
+  static ThemeData _getInitialTheme() {
+    final brightness =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness;
+    return brightness == Brightness.dark ? darkMode : lightMode;
+  }
 
 //state.brightness only can be dark or light, and this tells us wich Theme
 //is currently active
