a8c32981cbed86dbb65576725bede2a7568bd3ae
Felix <felixdressler@freenet.de>
2024-08-12 07:34:46 +0200

    Category / Contract : toMap / fromMap

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -3,4 +3,18 @@ class Category {
   String description;
 
   Category({required this.id, required this.description});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'] as int,
+      description: map['description'] as String,
+    );
+  }
 }
--- lib/model/contract.dart
+++ lib/model/contract.dart
@@ -17,4 +17,26 @@ class Contract {
     this.income = false,
     required this.amount,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'billingPeriod': billingPeriod.index,
+      'category': category,
+      'income': income,
+      'amount': amount,
+    };
+  }
+
+  factory Contract.fromMap(Map<String, dynamic> map) {
+    return Contract(
+      id: map['id'] as int,
+      description: map['description'] as String,
+      billingPeriod: BillingPeriod.values[map['billing_period'] as int],
+      category: Category.fromMap(map['category'] as Map<String, dynamic>),
+      income: map['income'] as bool,
+      amount: map['amount'] as int,
+    );
+  }
 }
